d9dcbb609a69b164897a4b271796335b1cc30874
Amitai Burstein <amitai@gizra.com>
2013-04-29 22:52:08 +0300

    Fix scroll.

--- assets/themes/twitter/js/frontpage.js
+++ assets/themes/twitter/js/frontpage.js
@@ -1,16 +1,23 @@
 jQuery(document).ready(function($) {
 
   $(".scroll").click(function(event){
-
     // Split the hash form the "#".
     var target = this.hash.split("#");
     target = target[1];
 
     // Get the top offset of the target anchor
-    var $targetTop = $('a[name=' + target + ']').offset().top - 100;
-    console.log($targetTop);
+    var $targetTop = $('a[name=' + target + ']').offset().top;
 
     event.preventDefault();
+
+    // Make sure the fragment is updated in the URL.
+    history.pushState(null, '', '#' + target);
     $('html,body').animate({scrollTop: $targetTop}, 500);
   });
+
+  if (window.location.hash) {
+    var target = window.location.hash.split("#");
+    target = target[1];
+    // $('header a[href$="'+ target + '"]').trigger('click');
+  }
 });
