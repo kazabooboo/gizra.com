5eb98c5468d0ab579908c181b6b999efc78c951f
Amitai Burstein <amitai@gizra.com>
2014-06-01 16:31:29 -0500

    tags'

--- Gruntfile.js
+++ Gruntfile.js
@@ -248,6 +248,7 @@ module.exports = function (grunt) {
             // Jekyll processes and moves HTML and text files.
             // Usemin moves CSS and javascript inside of Usemin blocks.
             // Copy moves asset files and directories.
+            'assets/**/*',
             'images/**/*',
             'fonts/**/*',
             // Like Jekyll, exclude files & folders prefixed with an underscore.
