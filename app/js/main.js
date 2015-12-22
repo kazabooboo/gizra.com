47737021f554b8a09a19a01b9f036b4684523ba0
YaronMiro <yaron@gizra.com>
2015-12-22 12:22:29 +0200

    Merge branch #237 into master

--- app/js/main.js
+++ app/js/main.js
@@ -1,48 +1,89 @@
 $(function() {
-
   // About-story images.
   if (navigator.userAgent.match(/(iPod|iPhone|iPad)/)) {
-    $('.section2').backstretch("/assets/images/about-story/lamp.jpg");
-    $('.section4').backstretch("/assets/images/about-story/fish.jpg");
+    $('.section2').backstretch('/assets/images/about-story/lamp.jpg');
+    $('.section4').backstretch('/assets/images/about-story/fish.jpg');
   }
 });
 
 $(document).ready(function(){
-  // Check if the current device support touch.
+
+  /**
+   * Smooth scroll.
+   *
+   */
+  $('a.smooth-scroll-to').click(function(event) {
+    $('html, body').animate({
+      scrollTop: $(this.hash).offset().top
+    }, 800);
+    event.preventDefault();
+  });
+
+  /**
+   * Check if the current device support touch.
+   *
+   */
   function is_touch_device() {
     return 'ontouchstart' in window || 'onmsgesturechange' in window;
   }
 
-  // In case we are on a "mobile" device.
-  if (is_touch_device()) {
+  /**
+   * Touch handler function for the "clients" logos on the homepage.
+   *
+   */
+  var clientsLogosOnTouch = function() {
+    var selectorsList = '.popup-block, img';
 
-    // Display an info icon overlaying the member image.
-    $('#team-page .icon-info').show();
+    // Current logo being touch.
+    var $targetLogo = $(this);
 
-    // Toggle the member "info" block to "fade in"/"fade out" on each touch.
-    $("#team-page .member-box .icon-info").bind( "touchstart", function() {
-      $(this).parent('.member-box').find('.popup-block').fadeToggle( "slow", "linear" );
-    });
+    // Hide previous open logo as long as we are not pointing to our self.
+    var $openLogo = $('#work .clients .logo .popup-block:visible').parent('.logo');
+    if ($openLogo.length && $openLogo[0] !== $targetLogo[0]) {
+      $openLogo.find(selectorsList).fadeToggle('fast', 'linear')
+    }
+
+    // Toggle the selected logo.
+    $targetLogo.find(selectorsList).fadeToggle('fast', 'linear');
   }
-  // In case we are on "desktop" device.
-  else {
-    // Display the client "info" block.
-    $("#work .clients li").has('.popup-block').hoverIntent(
+
+  /**
+   * No touch handler function for the "clients" logos on the homepage.
+   *
+   */
+  function clientsLogosOnHover() {
+    // Display the client 'info' block.
+    $('#work .clients li').has('.popup-block').hoverIntent(
       // While hovering the target element.
       function(){
-        $(this).find('img').fadeOut();
-        $(this).find('.popup-block').fadeIn();
+        $(this).find('.popup-block, img').fadeToggle('fast', 'linear');
       },
       // After we stop to hover over the target element.
       function(){
-        $(this).find('.popup-block').fadeOut();
-        $(this).find('img').fadeIn();
+        $(this).find('.popup-block, img').fadeToggle('fast', 'linear');
       }
     )
+  }
 
-    // Display the member info block once("hoverIntent")
+  /**
+   * Touch handler for the team "member" on the team page.
+   *
+   */
+  function teamMemberInfoOnTouch()  {
+    // Toggle the member 'info' block to 'fade in'/'fade out' on each touch.
+    $('#team-page .member-box .icon-info').bind( 'touchstart', function() {
+      $(this).parent('.member-box').find('.popup-block').fadeToggle( 'slow');
+    })
+  }
+
+  /**
+   * No touch handler for the team "member" on the team page.
+   *
+   */
+  function teamMemberInfoOnHover()  {
+    // Display the member info block once('hoverIntent')
     // only while hovering the member image.
-    $("#team-page .member-box").has('.popup-block').hoverIntent(
+    $('#team-page .member-box').has('.popup-block').hoverIntent(
       // While hovering the target element.
       function(){
         $(this).find('.popup-block').fadeIn();
@@ -53,4 +94,22 @@ $(document).ready(function(){
       }
     )
   }
+
+  // In case we have a devices that support touch.
+  if (is_touch_device()) {
+    // Adding touch class to the body tag if it's a touch device.
+    $('body').addClass('is-touch-device');
+
+    // Toggle the member 'info' block to 'fade in'/'fade out' on each touch.
+    teamMemberInfoOnTouch();
+
+    // Toggle the client 'info' block to 'fade in'/'fade out' on each touch.
+    $('#work .clients .logo').bind( 'touchstart', clientsLogosOnTouch)
+  }
+  // In case we have a devices that doesn't support touch.
+  else {
+    clientsLogosOnHover();
+    teamMemberInfoOnHover();
+  }
+
 });
